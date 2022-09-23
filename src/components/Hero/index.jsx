0c5237fbfd91d1111df2a501b801d07b05617a73
Geovana Grigorio <geovanagomes832@gmail.com>
2022-09-22 21:52:50 -0300

    Add files via upload

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { Button } from '../ButtonElements'
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1> MakeUp Make Easy</HeroH1>
+        <HeroP>Sign up for a new account today and recive $25 in credit towards your next purchase</HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'               smooth={true} duration={500} spy={true} exact='true' offset={-80}>
+            Get Ready {hover ? <ArrowForward /> : <ArrowRight/>}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default Hero
